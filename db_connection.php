f2cfb0ab8e5056e1934a6081f24561d0df0fd830
johnneppel96 <43040925+johnneppel96@users.noreply.github.com>
2019-08-26 13:32:16 -0400

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,8 +1,8 @@
 
 <?php
 //This file is kept within a private directory on the server.
-$username = 'root';
-$password = '!J0hnnymax';
+$username = '';
+$password = '';
 $db_name = 'tweet_trends_data';         //URL below
 global $db_connection;
 $db_connection = new mysqli('localhost',$username, $password, $db_name) or die('something broke');
